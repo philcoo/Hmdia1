41f2809356b134ada97fb9348ab097cb81afcafd
phil <jpme4631@gmail.com>
2018-10-03 20:54:25 -0400

    landing page be resopive now

--- src/components/home.js
+++ src/components/home.js
@@ -1,7 +1,6 @@
 import React from "react";
 import {Grid, Cell} from 'react-mdl';
-import {Link} from "react-router-dom"
-
+import {Link} from "react-router-dom";
   const Home = () =>{
     return(
       <div className="home-grid" style={{width:'100%', margin: 'auto'}}>
@@ -17,14 +16,28 @@ import {Link} from "react-router-dom"
           <h2 className="trending" >Trend/Sak-Sho</h2>
 
           {/* shows grid here, these are the trending events*/}
-    <Grid className="home-trend" style={{width:'100%'}}>
-        <Cell col={3}><img src="jessie1.jpg"/></Cell>
-        <Cell col={3}><img src="jean.jpg"/></Cell>
-        <Cell col={3}>
+    <Grid className="home-trend">
+
+        <Cell className="trenditem" >
+          <img src="jessie1.jpg"/>
+        </Cell>
+
+        <Cell className="trenditem" >
           <Link to="./show1"> <img src="jessie1.jpg"/> </Link>
        </Cell>
 
-          <Cell col={3}><img src="sejo.jpg"/></Cell>
+       <Cell className="trenditem" >
+         <Link to="./show1"> <img src="jessie1.jpg"/> </Link>
+
+      </Cell>
+
+
+      <Cell className="trenditem" >
+        <Link to="./show1"> <img src="jessie1.jpg"/> </Link>
+     </Cell>
+
+     
+
     </Grid>
 
       </div>
