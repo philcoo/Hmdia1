1a5303dcfab7abbe0e6257b3bab0e7cdbc7b34c4
phil <jpme4631@gmail.com>
2018-10-03 23:24:48 -0400

    landing page be resopive now and bacground change

--- src/components/home.js
+++ src/components/home.js
@@ -1,45 +1,37 @@
 import React from "react";
-import {Grid, Cell} from 'react-mdl';
+import {Grid, Cell, title} from 'react-mdl';
 import {Link} from "react-router-dom";
   const Home = () =>{
     return(
+
+
+
       <div className="home-grid" style={{width:'100%', margin: 'auto'}}>
-
           <img src="successmom.jpg"/>
+          {/* trending content*/}
+           <h2 className="trending" >Trend/Sak-Sho</h2>
+              {/* shows grid here, these are the trending events*/}
+          <Grid className="home-trend" style={{width:'100%', margin: 'auto'}}>
+                      <Cell className="trenditem" >
+                        <img src="guy.jpg"/>
+                        
+                      </Cell>
 
-        <div>
+                      <Cell className="trenditem" >
+                        <Link to="./show1"> <img src="jean.jpg"/> </Link>
+                     </Cell>
 
-        </div>
+                     <Cell className="trenditem" >
+                       <Link to="./show1"> <img src="sejo.jpg"/> </Link>
 
-        {/* trending content*/}
-
-          <h2 className="trending" >Trend/Sak-Sho</h2>
-
-          {/* shows grid here, these are the trending events*/}
-    <Grid className="home-trend">
-
-        <Cell className="trenditem" >
-          <img src="jessie1.jpg"/>
-        </Cell>
-
-        <Cell className="trenditem" >
-          <Link to="./show1"> <img src="jessie1.jpg"/> </Link>
-       </Cell>
-
-       <Cell className="trenditem" >
-         <Link to="./show1"> <img src="jessie1.jpg"/> </Link>
-
-      </Cell>
+                    </Cell>
 
 
-      <Cell className="trenditem" >
-        <Link to="./show1"> <img src="jessie1.jpg"/> </Link>
-     </Cell>
+                    <Cell className="trenditem" >
+                      <Link to="./show1"> <img src="jessie1.jpg"/> </Link>
 
-     
-
-    </Grid>
-
+                   </Cell>
+          </Grid>
       </div>
     );
   };
